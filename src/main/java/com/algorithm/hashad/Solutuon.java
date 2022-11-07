531d442c308c77efeff6bff461ec9c144ec3e644
S2ujeong <sj980204@gmail.com>
2022-11-07 09:32:41 +0900

    하샤드 수

---
+++ src/main/java/com/algorithm/hashad/Solutuon.java
@@ -0,0 +1,25 @@
+package com.algorithm.hashad;
+
+public class Solutuon {
+
+    public boolean solution(int x) {
+        // 자릿수의 합 구하기
+        int sumOfDigit = 0;
+        while (x > 0) {
+            sumOfDigit += x % 10;
+            x = x / 10;
+        }
+        // 나누어 떨어지는 지 확인
+        if ( (x % sumOfDigit) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Solutuon s = new Solutuon();
+        System.out.println(s.solution(18));
+
+    }
+}
