715f6d8833baa7be45a295b2967cef7485fc6a59
S2ujeong <sj980204@gmail.com>
2022-11-17 16:31:07 +0900

    대소문자 구분 후 문제풀이 완료

--- src/main/java/algorithmLecture/string/FindChar.java
+++ src/main/java/algorithmLecture/string/FindChar.java
@@ -2,10 +2,21 @@ package algorithmLecture.string;
 
 import java.util.Scanner;
 
-// 대소문자 구분하지 않는다.
+// 대소문자 구분하지 않는다. -> 대/소문자 중 하나로 통일시킨다.
 public class FindChar {
-    public int solution(String srt, char t) {
+    public int solution(String strInput, char target) {
         int answer = 0;
+        strInput = strInput.toUpperCase();
+        target = Character.toUpperCase(target);
+
+        System.out.println(strInput);
+        System.out.println(target);
+
+        for (int i = 0; i < strInput.length(); i++) {
+            if (strInput.charAt(i) == target ) {
+                answer++;
+            }
+        }
 
         return answer;
     }
@@ -14,14 +25,7 @@ public class FindChar {
         FindChar m = new FindChar();
         Scanner sc = new Scanner(System.in);
         String strInput = sc.next();
-        char target = strInput.charAt(0);
-        int answer = 1;  // target 값은 어차피 같을 테니 1개 미리 올려두고, for문 index는 0이 아닌 1부터 시작
-
-        for (int i = 1; i < strInput.length(); i++) {
-            if (strInput.charAt(i) == target ) {
-                answer++;
-            }
-        }
-        System.out.println("입력된 문자: " + strInput + "정답은 ?:" + answer);
+        char target = sc.next().charAt(0);
+        System.out.println("입력된 문자: " + strInput + "정답은 ?: " + m.solution(strInput,target) );
     }
 }
