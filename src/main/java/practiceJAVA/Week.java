e4fc40431097a1d27585ab316916cffecb5ceb52
sujeong <sj980204@gmail.com>
2023-08-22 12:45:20 +0900

    🚀[feat] 자바 - 열거형(Enum)

---
+++ src/main/java/practiceJAVA/Week.java
@@ -0,0 +1,14 @@
+package practiceJAVA;
+
+public enum Week {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+    FREE_DAY
+}
+
+// 다른 클래스에서 사용할 때에는, 클래스명을 변수 타입으로 생각하고 선언하면 된다. Week today = Week.FREE_DAY;
