ea2e2af99db3098d948401c218e695454dff2071
sujeong <sj980204@gmail.com>
2024-12-10 16:25:43 +0900

    [code] java 입출력, 파싱 정석

---
+++ src/main/java/template/io/Main.java
@@ -0,0 +1,33 @@
+package template.io;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.valueOf(st.nextToken());
+        int l = Integer.valueOf(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.valueOf(st.nextToken());
+        }
+
+        logic();
+
+        bw.write("Hello");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static void logic() {
+        System.out.println("logic");
+    }
+}
